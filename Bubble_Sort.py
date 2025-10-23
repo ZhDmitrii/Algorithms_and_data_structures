489f9ff9ca8696bf9110567097ad25e9c2e717d0
ZhDmitrii <dmitrii_zhigailo@mail.ru>
2025-10-23 20:37:36 +0300

    Сортировка пузырьками 1

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -1,13 +1,13 @@
 def bubble_sort(arr):
-    n = len(arr) - 1
-    for i in range(n):
-        for j in range(n - i):
+    n = len(arr)
+    for i in range(0, n - 1):
+        for j in range(0, n - 1 - i):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 
-my_list = [64, 34, 25, 12, 22, 11, 90]
-# my_list = input().split()
-# my_list = list(map(int, my_list))
+# my_list = [64, 34, 25, 12, 22, 11, 90]
+my_list = input().split(",")
+my_list = list(map(int, my_list))
 bubble_sort(my_list)
 print("Отсортированный список:", my_list)
